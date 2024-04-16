02ed280c2ea3f7f962ce8030d435053f4bdfee02
GioCarmona <gcarmon1@ibero.edu.co>
2024-04-15 20:27:58 -0500

    Actualización Javscript actividad

--- SCRIPTS/practicaConceptos.js
+++ SCRIPTS/practicaConceptos.js
@@ -1,1 +1,24 @@
 const edadInput = document.getElementById("edadInput");
+/*console.log(edadInput);*/
+/*Se puede hacer mediante constantes y se traen con ID - const button = document.getElementById("boton");
+console.log(boton)*/
+const mensaje = document.getElementById("mensaje");
+
+function verificarEdad(){
+    //Tambien se puede llamar por eventos con "function"
+    //cada que le de click al boton arroje un mensaje de "Hola"
+    //console-console.log(("Hola"));
+    let edad = edadInput.value;
+    if (edad >= 18) {
+        //console.log("Eres mayor de edad")
+        mensaje.textContent = "Eres mayor de edad";        
+    }else{
+        //console.log("No eres mayor de edad")
+        mensaje.textContent = "No eres mayor de edad";
+    }
+    //console.log(edad)    
+}
+
+
+
+
